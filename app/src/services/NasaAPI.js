2c1f8390cdd59b9b1c859d9fc70932d42b53cdce
Fernando Neirot <fernando.neirot@hotmail.com>
2022-05-16 17:19:56 -0300

    paso apikey a una variable de entorno

--- app/src/services/NasaAPI.js
+++ app/src/services/NasaAPI.js
@@ -2,8 +2,10 @@
 
 import axios from "axios"
 
+const apiKey=process.env.REACT_APP_API_KEY_NASA;
+
 export const getPhotos = async() => {
-    const res = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=rRxPyohPD3XnYawgbZQWtS2cGi9d4wImpy064dpj`)
+    const res = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
     .then(res=>{
         return res.data;
     })
